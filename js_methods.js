6c0bbade64d982fedea2a305dfb97d7cdfd1f4bf
Kaitlyn Zhen <kzhen7923@gmail.com>
2022-03-01 14:10:30 -0500

    added myEach and myMap

---
+++ js_methods.js
@@ -0,0 +1,55 @@
+// FOR EACH //
+Array.prototype.myEach = function(callback) {
+    for(let index = 0; index < this.length; index++) {
+        callback(this[index],index,this);
+    }
+};
+  
+  // MAP //
+  Array.prototype.myMap = function(callback) {
+    const newArray = [];
+    for(let index = 0; index < this.length; index++) {
+        newArray.push(callback(this[index], index, this));
+    }
+    return newArray;
+};
+  
+  // SOME //
+  Array.prototype.mySome = function() {
+    // Place your code here.
+  };
+  
+  // REDUCE //
+  Array.prototype.myReduce = function() {
+    // Place your code here.
+  };
+  
+  // INCLUDES //
+  Array.prototype.myIncludes = function() {
+    // Place your code here.
+  };
+  
+  // INDEXOF //
+  Array.prototype.myIndexOf = function() {
+    // Place your code here.
+  };
+  
+  // PUSH //
+  Array.prototype.myPush = function() {
+    // Place your code here.
+  };
+  
+  // LASTINDEXOF //
+  Array.prototype.myLastIndexOf = function() {
+    // Place your code here.
+  };
+  
+  // KEYS //
+  Object.myKeys = function() {
+    // Place your code here.
+  };
+  
+  // VALUES //
+  Object.myValues = function() {
+    // Place your code here.
+  };
