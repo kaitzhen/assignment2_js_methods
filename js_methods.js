2d4a8795e9cf243b1d2f64d73e30c92541ed739e
Kaitlyn Zhen <kzhen7923@gmail.com>
2022-03-02 12:32:32 -0500

    add myIncludes

--- js_methods.js
+++ js_methods.js
@@ -37,9 +37,28 @@ Array.prototype.myReduce = function(callback,initVal) {
 };
   
   // INCLUDES //
-  Array.prototype.myIncludes = function() {
-    // Place your code here.
-  };
+Array.prototype.myIncludes = function(searchElement,fromIndex=0) {
+  //console.log(`given fromIndex = ${fromIndex}`);
+  //console.log(`array length = ${array.length}`);
+
+  if(this.length==0) {   //if array empty
+      return false;
+  }
+  if(fromIndex < 0) {             //edge cases for fromIndex
+      fromIndex+=this.length;
+      if(fromIndex<0) {
+          fromIndex = 0;
+      }
+  } else if(fromIndex > this.length) {
+      return false;
+  }
+  for(let index=fromIndex; index<this.length; index++) {
+      if(this[index] == searchElement) {
+          return true;
+      } 
+  }
+  return false;
+};
   
   // INDEXOF //
   Array.prototype.myIndexOf = function() {
