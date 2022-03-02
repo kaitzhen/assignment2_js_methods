cff7864575957af542c80592a12c385f13a4ea43
Kaitlyn Zhen <kzhen7923@gmail.com>
2022-03-02 12:33:43 -0500

    added myPush

--- js_methods.js
+++ js_methods.js
@@ -87,9 +87,16 @@ Array.prototype.myIndexOf = function(searchElement,fromIndex=0) {
 
   
   // PUSH //
-  Array.prototype.myPush = function() {
-    // Place your code here.
-  };
+Array.prototype.myPush = function(...args) {
+  let args_index = 0;  // Index of new element
+  let length = this.length;   // Length of "this" array
+  // The last element of "this" array is at length - 1
+  for (let i = length; i < length + args.length; i++) {
+      this[i] = args[args_index];  // Add new element to end of "this" array
+      args_index++;
+  }
+  return this.length;  // Return new length of "this" array
+};
   
   // LASTINDEXOF //
   Array.prototype.myLastIndexOf = function() {
